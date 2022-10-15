0f17de93736e36c76fec8e2b87663b9d8bab8f5f
TristinParker <gidgidonihah@byui.edu>
2022-10-14 20:33:34 -0600

    Base oath setup and frontend

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 require("dotenv").config();
 
 const client = process.env.github_ClientId;
-
+console.log(client);
 routes.get('/',(req,res) =>{
     res.redirect(
         `https://github.com/login/oauth/authorize?client_id=${client}`
