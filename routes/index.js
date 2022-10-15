fab26e04baa7b675a5c0fa5de94a86bfd2bebef4
TristinParker <gidgidonihah@byui.edu>
2022-10-15 13:22:29 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ const routes = require('express').Router();
 const fs = require('fs');
 
 routes.use('/', require('./swagger'));
-
-//routes.use('/oauth', require('./oauth'));
-//routes.use('/oauth-callback', require('./oauth-callback'));
-routes.use('/agents', require('./agents'));
-//routes.use('/wishlist', require('./wishlist'));
 routes.use('/',(req,res)=>{
     const htmlFile= './main/index.html';
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     fs.createReadStream(htmlFile).pipe(res);
 });
+//routes.use('/oauth', require('./oauth'));
+//routes.use('/oauth-callback', require('./oauth-callback'));
+routes.use('/agents', require('./agents'));
+//routes.use('/wishlist', require('./wishlist'));
+
 
 
 
