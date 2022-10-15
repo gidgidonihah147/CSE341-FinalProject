af35b7290c8dd677ceb863a2bebbd699c740a2fa
TristinParker <gidgidonihah@byui.edu>
2022-10-14 19:10:25 -0600

    oath setup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const routes = require('express').Router();
+const fs = require('fs');
 
-routes.use('/movies', require('./movies'))
-routes.use('/wishlist', require('./wishlist'))
-routes.use('/swagger', require('./swagger'));
-routes.use('/',(req, res) => {
-    res.sendFile(path.join(__dirname,'../index.html'));
+routes.use('/', require('./swagger'));
+
+routes.use('/oauth', require('./oauth'));
+routes.use('/oauth-callback', require('./oauth-callback'));
+routes.use('/movies', require('./movies'));
+routes.use('/wishlist', require('./wishlist'));
+routes.use('/',(req,res)=>{
+    const htmlFile= './main/index.html';
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    fs.createReadStream(htmlFile).pipe(res);
 });
+
+
 
 
 
