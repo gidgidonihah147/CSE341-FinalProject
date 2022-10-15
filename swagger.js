6af7457e5cdf7c4e6be1ee4c91d82d156d3a8ec6
dosmaster4life <jefferysuperman@gmail.com>
2022-10-14 21:41:55 -0600

    bug fixes

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
     info: {
         title: 'CSE341-Project2',
-        description: 'Swagger API Documentation for project 2 assigment for CSE 341 - Tristin Parker'
+        description: 'Swagger API Documentation for the final projec'
     },
     host: 'homesforsalenow.onrender.com',
     schemes: ['https']
